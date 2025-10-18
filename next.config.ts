ed1a8b552c1f28e7b005a49eff928541554a0bfa
Suraj Reddy Annu <surajreddy771@gmail.com>
2025-10-18 16:33:37 +0000

    Of course. I will update the "Financial Literacy Corner" with the new hy

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -23,25 +24,9 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
-     dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    unoptimized: false,
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    formats: ['image/webp'],
-    loader: 'default',
-    path: '/_next/image',
-    domains: [],
-    disableStaticImages: false,
-    minimumCacheTTL: 60,
-    remotePatterns: [
-      {
-        protocol: 'data',
-        hostname: '**',
-      },
-    ],
   },
 };
 
 export default nextConfig;
+
+    
